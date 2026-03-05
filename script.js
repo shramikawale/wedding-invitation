64f85f142d29bce07b41794c8a92736ad44b234e
Shramik <sawale@express.com>
2026-03-05 16:44:41 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
-// Open Video Popup
-const gif = document.getElementById("gifPreview");
-const modal = document.getElementById("videoModal");
-const closeBtn = document.querySelector(".close");
+// Play music when user clicks anywhere (browser required)
 
-gif.addEventListener("click", () => {
-modal.style.display = "flex";
-});
-
-// Close Video
-closeBtn.addEventListener("click", () => {
-modal.style.display = "none";
-});
-
-// Play Music When User Clicks Anywhere
-document.body.addEventListener("click", function(){
-const music = document.getElementById("bgMusic");
-music.play();
+document.addEventListener("click", function () {
+  const music = document.getElementById("bgMusic");
+  music.play();
 }, { once: true });
